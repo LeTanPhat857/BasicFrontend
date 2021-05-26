e6293baa28195bb3e2797db2949415cb1bbfaef2
LeTanPhat <letanphat857@gmail.com>
2021-05-27 01:56:53 +0700

    add service

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HelloComponent } from './hello/hello.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HelloComponent
+    HelloComponent,
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { Heroes } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(Heroes);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
+
+  constructor(private messageService: MessageService) { }
+}
